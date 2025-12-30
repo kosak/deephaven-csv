6de2288721420f36b8c601abaf4307ee00128d66
Corey Kosak <coreykosak@deephaven.io>
2025-12-29 19:19:32 -0500

    blah

--- src/test/java/io/deephaven/csv/MultipleCustomParsersTest.java
+++ src/test/java/io/deephaven/csv/MultipleCustomParsersTest.java
@@ -1,5 +1,6 @@
 package io.deephaven.csv;
 
+import io.deephaven.csv.parsers.Parser;
 import io.deephaven.csv.parsers.Parsers;
 import io.deephaven.csv.testutil.Column;
 import io.deephaven.csv.testutil.ColumnSet;
@@ -7,20 +8,43 @@ import io.deephaven.csv.testutil.CsvTestUtil;
 import io.deephaven.csv.util.CsvReaderException;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultipleCustomParsersTest {
     @Test
-    public void bug70() throws CsvReaderException {
-        final String input = "Key,0to3Hearts,2To4Hearts,5Hearts\n" +
+    public void zeroToThreeHearts() throws CsvReaderException {
+        final String input = "Key,Value\n" +
                 "A,hello\n" +
                 "B,❤hello\n" +
                 "C,❤hello❤\n" +
-                "C,❤he❤art3❤\n";
+                "C,❤he❤llo❤\n";
+
+
 
         final ColumnSet expected =
                 ColumnSet.of(
-                        Column.ofRefs("Coin", "USDT", "USDT", "USDT", "USDT"),
-                        Column.ofValues("Change", -49.00787612, -152.686844, -59.92650232, -102.3862566),
-                        Column.ofRefs("Remark", null, "穿仓保证金补偿", null, null));
-        CsvTestUtil.invokeTest(CsvTestUtil.defaultCsvBuilder().parsers(Parsers.DEFAULT).build(), input, expected);
+                        Column.ofRefs("Key", "A", "B", "C", "D"),
+                        Column.ofRefs("Value",
+                                new ZeroThroughThreeValue("hello"),
+                                new ZeroThroughThreeValue("❤hello"),
+                                new ZeroThroughThreeValue("❤hello❤"),
+                                new ZeroThroughThreeValue("❤he❤llo❤")
+                        ));
+
+        CsvTestUtil.invokeTest(csvSpecsWithHearts(), input, expected);
+    }
+
+    private static CsvSpecs csvSpecsWithHearts() {
+        HeartParser zeroThroughThree = new HeartParser(0, 3, s -> new ZeroThroughThreeValue(s));
+        HeartParser twoThroughFour = new HeartParser(2, 4, s -> new TwoThroughFourValue(s));
+        HeartParser zeroThroughFive = new HeartParser(0, 5, s -> new ZeroThroughFiveValue(s));
+
+        List<Parser<?>> parsers = new ArrayList<>(Parsers.DEFAULT);
+        parsers.add(zeroThroughThree);
+        parsers.add(twoThroughFour);
+        parsers.add(zeroThroughFive);
+
+        return CsvTestUtil.defaultCsvBuilder().parsers(parsers).build();
     }
 }
