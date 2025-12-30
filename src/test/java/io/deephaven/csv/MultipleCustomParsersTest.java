253a67de4f20092a9c357b4177c73888a59a9210
Corey Kosak <coreykosak@deephaven.io>
2025-12-29 19:31:11 -0500

    ok this is going to be good

--- src/test/java/io/deephaven/csv/MultipleCustomParsersTest.java
+++ src/test/java/io/deephaven/csv/MultipleCustomParsersTest.java
@@ -21,91 +21,57 @@ public class MultipleCustomParsersTest {
                 "C,❤hello❤\n" +
                 "C,❤he❤llo❤\n";
 
-
-
         final ColumnSet expected =
                 ColumnSet.of(
                         Column.ofRefs("Key", "A", "B", "C", "D"),
                         Column.ofRefs("Value",
-                                new ZeroThroughThreeValue("hello"),
-                                new ZeroThroughThreeValue("❤hello"),
-                                new ZeroThroughThreeValue("❤hello❤"),
-                                new ZeroThroughThreeValue("❤he❤llo❤")
+                                TaggedHeartValue.of("hello", HeartCategory.ZERO_THROUGH_THREE),
+                                TaggedHeartValue.of("❤hello", HeartCategory.ZERO_THROUGH_THREE),
+                                TaggedHeartValue.of("❤hello❤", HeartCategory.ZERO_THROUGH_THREE),
+                                TaggedHeartValue.of("❤he❤llo❤", HeartCategory.ZERO_THROUGH_THREE)
                         ));
 
         CsvTestUtil.invokeTest(csvSpecsWithHearts(), input, expected);
     }
 
     private static CsvSpecs csvSpecsWithHearts() {
-        HeartParser zeroThroughThreeParser = new HeartParser(0, 3, s -> new ZeroThroughThreeValue(s));
-        HeartParser twoThroughFourParser = new HeartParser(2, 4, s -> new TwoThroughFourValue(s));
-        HeartParser zeroThroughFivParser = new HeartParser(0, 5, s -> new ZeroThroughFiveValue(s));
+        HeartParser zeroThroughThreeParser = new HeartParser(0, 3, HeartCategory.ZERO_THROUGH_THREE);
+        HeartParser twoThroughFourParser = new HeartParser(2, 4, HeartCategory.TWO_THROUGH_FOUR);
+        HeartParser zeroThroughFiveParser = new HeartParser(0, 5, HeartCategory.ZERO_THROUGH_FIVE);
 
         List<Parser<?>> parsers = new ArrayList<>(Parsers.DEFAULT);
-        parsers.add(zeroThroughThree);
-        parsers.add(twoThroughFour);
-        parsers.add(zeroThroughFive);
+        parsers.add(zeroThroughThreeParser);
+        parsers.add(twoThroughFourParser);
+        parsers.add(zeroThroughFiveParser);
 
         return CsvTestUtil.defaultCsvBuilder().parsers(parsers).build();
     }
 
-    private static final class ZeroThroughThreeValue {
-        private final String text;
+    private static final class HeartParser implements Parser<TaggedHeartValue> {
 
-        public ZeroThroughThreeValue(String text) {
-            this.text = text;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass()) return false;
-            ZeroThroughThreeValue that = (ZeroThroughThreeValue) o;
-            return Objects.equals(text, that.text);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(text);
-        }
     }
 
-    private static final class TwoThroughFourValue {
+    private enum HeartCategory { ZERO_THROUGH_THREE, TWO_THROUGH_FOUR, ZERO_THROUGH_FIVE };
+
+    private static final class TaggedHeartValue {
+        private final HeartCategory heartCategory;
         private final String text;
 
-        public TwoThroughFourValue(String text) {
+        public TaggedHeartValue(HeartCategory heartCategory, String text) {
+            this.heartCategory = heartCategory;
             this.text = text;
         }
 
         @Override
         public boolean equals(Object o) {
             if (o == null || getClass() != o.getClass()) return false;
-            TwoThroughFourValue that = (TwoThroughFourValue) o;
-            return Objects.equals(text, that.text);
+            TaggedHeartValue that = (TaggedHeartValue) o;
+            return heartCategory == that.heartCategory && Objects.equals(text, that.text);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hashCode(text);
-        }
-    }
-
-    private static final class ZeroThroughFiveValue {
-        private final String text;
-
-        public ZeroThroughFiveValue(String text) {
-            this.text = text;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass()) return false;
-            ZeroThroughFiveValue that = (ZeroThroughFiveValue) o;
-            return Objects.equals(text, that.text);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(text);
+            return Objects.hash(heartCategory, text);
         }
     }
 }
