63d8465df82fef59fcc28052ea8748c51d609f65
Corey Kosak <coreykosak@deephaven.io>
2026-01-05 18:10:02 -0500

    get rid of this moveable thing

--- src/main/java/io/deephaven/csv/parsers/IteratorHolder.java
+++ src/main/java/io/deephaven/csv/parsers/IteratorHolder.java
@@ -3,7 +3,6 @@ package io.deephaven.csv.parsers;
 import io.deephaven.csv.containers.ByteSlice;
 import io.deephaven.csv.densestorage.DenseStorageReader;
 import io.deephaven.csv.util.CsvReaderException;
-import io.deephaven.csv.util.Moveable;
 
 /**
  * This class is used to hold the underlying {@link DenseStorageReader} plus some associated helper information (an
@@ -12,12 +11,12 @@ import io.deephaven.csv.util.Moveable;
  */
 public final class IteratorHolder {
     /** The {@link DenseStorageReader} for the input text. */
-    private final DenseStorageReader dsr;
+    private DenseStorageReader dsr;
     /**
      * Storage for our reusable byte slice. Data inside it is valid after a call to tryMoveNext() returns true, in the
      * case where hasBytes has been set to true.
      */
-    private final ByteSlice bs = new ByteSlice();
+    private ByteSlice bs = new ByteSlice();
     /** Number of successful calls so far to tryMoveNext (i.e. those that returned true). */
     private long numConsumed = 0;
     /** Valid anytime after the first call to tryMoveNext(), but not before. */
@@ -30,6 +29,18 @@ public final class IteratorHolder {
      */
     public IteratorHolder(DenseStorageReader dsr) {
         this.dsr = dsr;
+    }
+
+    /**
+     * Makes a copy of this {@link IteratorHolder}. Requires that the {@link IteratorHolder} be
+     * in its initial state, i.e. that {@link IteratorHolder#tryMoveNext()} has never been called on it.
+     * @return The new {@link IteratorHolder}.
+     */
+    public IteratorHolder copyMustBeFresh() {
+        if (isExhausted || numConsumed != 0) {
+            throw new RuntimeException("Can't copy IteratorHolder that is not in its initial state");
+        }
+        return new IteratorHolder(dsr.copy());
     }
 
     /**
@@ -58,6 +69,11 @@ public final class IteratorHolder {
         throw new CsvReaderException("Iteration exhausted");
     }
 
+    public void release() {
+        dsr = null;
+        bs = null;
+    }
+
     /**
      * Getter for the byte slice.
      * 
@@ -84,13 +100,5 @@ public final class IteratorHolder {
      */
     public boolean isExhausted() {
         return isExhausted;
-    }
-
-    /**
-     * The underlying DenseStorageReader
-     * @return The underlying DenseStorageReader
-     */
-    public DenseStorageReader dsr() {
-        return dsr;
     }
 }
