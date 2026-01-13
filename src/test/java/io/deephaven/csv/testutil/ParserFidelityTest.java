5b8fa08c4ba25eb7bc613a8db2b1af9e2ea8a6e7
Corey Kosak <coreykosak@deephaven.io>
2026-01-12 22:17:21 -0500

    parser fidelity test

--- src/test/java/io/deephaven/csv/testutil/ParserFidelityTest.java
+++ src/test/java/io/deephaven/csv/testutil/ParserFidelityTest.java
@@ -6,14 +6,12 @@ import io.deephaven.csv.parsers.Parsers;
 import io.deephaven.csv.reading.CsvReader;
 import io.deephaven.csv.util.CsvReaderException;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.InputStream;
 import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Stream;
 
@@ -37,13 +35,38 @@ public class ParserFidelityTest {
     }
 
     private static Stream<Arguments> provideTuplesForTestFidelity() {
+        // Tuple format is: input, strictFloatFidelity, fastFloatFidelity, doubleFidelity
+
+        // Reminder:
+        // FloatStrictParser uses Float.parseFloat
+        // FloatFastParser uses a fast double parser (equivalent to Double.parseDouble) then casts to float
+        // DoubleParser uses a fast double parser (equivalent to Double.parseDouble)
         return Stream.of(
+                // CSV Parsers will agree with Java parsers
                 Arguments.of("NaN", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
+                Arguments.of("0", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
+                Arguments.of("-0.0", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
+                Arguments.of("+0.0", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
                 Arguments.of("-Infinity", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
                 Arguments.of("+Infinity", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
                 Arguments.of("0.1", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // FloatStrictParser will agree with Float.parseFloat
+                // FloatFastParser will fail because Double.parseDouble yields a number on this input that is a tad larger than Float.MAX_VALUE,
+                // so the FloatFastParser will refuse it (and there is no type inference to double in this test).
+                // DoubleParser will agree with Double.parseDouble
                 Arguments.of("3.4028235E38", ParseFidelity.SAME, ParseFidelity.PARSE_FAIL, ParseFidelity.SAME),
+                // CSV Parsers will agree with Java parsers
                 Arguments.of("1.4E-45", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                // FloatStrictParser will agree with Float.parseFloat
+                // FloatFastParser will succeed but disagree because (float)Double.parseDouble(input) yields a number
+                // that is a tad different from Float.parseFloat(input0
+                // DoubleParser will agree with Double.parseDouble
                 Arguments.of("1.4798235515974676E-17", ParseFidelity.SAME, ParseFidelity.DIFFERENT, ParseFidelity.SAME)
         );
     }
@@ -69,22 +92,25 @@ public class ParserFidelityTest {
 
         final CsvSpecs specs = CsvTestUtil.defaultCsvBuilder().parsers(Collections.singletonList(parser)).build();
         final InputStream stream = CsvTestUtil.toInputStream(source);
+        CsvReader.Result result;
         try {
-            CsvReader.Result result = CsvReader.read(specs, stream, CsvTestUtil.makeMySinkFactory());
-            Assertions.assertThat(result.numCols()).isEqualTo(1);
-            Assertions.assertThat(result.numRows()).isEqualTo(1);
-
-            final Object array = result.columns()[0].data();
-            final Object element0 = Array.get(array, 0);
-            boolean isSame = element0.equals(expectedValue);
-
-            if (isSame) {
-                Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.SAME);
-            } else {
-                Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.DIFFERENT);
-            }
+            result = CsvReader.read(specs, stream, CsvTestUtil.makeMySinkFactory());
         } catch (CsvReaderException e) {
             Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.PARSE_FAIL);
+            return;
+        }
+
+        Assertions.assertThat(result.numCols()).isEqualTo(1);
+        Assertions.assertThat(result.numRows()).isEqualTo(1);
+
+        final Object array = result.columns()[0].data();
+        final Object element0 = Array.get(array, 0);
+        boolean isSame = element0.equals(expectedValue);
+
+        if (isSame) {
+            Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.SAME);
+        } else {
+            Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.DIFFERENT);
         }
     }
 }
