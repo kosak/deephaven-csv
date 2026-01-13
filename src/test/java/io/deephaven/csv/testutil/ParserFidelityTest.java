a0ee0a56cc9ee4d259790c3b1f47f79230a15b61
Corey Kosak <coreykosak@deephaven.io>
2026-01-12 21:54:43 -0500

    I want the data baby

--- src/test/java/io/deephaven/csv/testutil/ParserFidelityTest.java
+++ src/test/java/io/deephaven/csv/testutil/ParserFidelityTest.java
@@ -1,8 +1,10 @@
 package io.deephaven.csv.testutil;
 
 import io.deephaven.csv.CsvSpecs;
+import io.deephaven.csv.parsers.Parser;
 import io.deephaven.csv.parsers.Parsers;
 import io.deephaven.csv.util.CsvReaderException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,70 +16,63 @@ import java.util.stream.Stream;
 
 public class ParserFidelityTest {
     public enum ParseFidelity {
-        /** The CSV Parser and the Java Parser return exactly the same value */
+        /**
+         * The CSV Parser and the Java Parser return exactly the same value
+         */
         SAME,
 
-        /** The CSV Parser and the Java Parser return different values */
+        /**
+         * The CSV Parser and the Java Parser return different values
+         */
         DIFFERENT,
 
-        /** The CSV Parser throws an exception */
+        /**
+         * The CSV Parser throws an exception
+         */
         PARSE_FAIL,
-    };
-
-    @ParameterizedTest
-    @MethodSource("provideTuplesForTestFidelity")
-    public void fidelity(String input, ParseFidelity strictFloatFidelity,
-                             ParseFidelity fastFloatFidelity, ParseFidelity doubleFidelity) throws CsvReaderException {
-
-        checkStrictFloat(input, strictFloatFidelity);
-        checkFastFloat(input, fastFloatFidelity);
-        checkDouble(input, doubleFidelity);
     }
 
     private static Stream<Arguments> provideTuplesForTestFidelity() {
         return Stream.of(
-                Arguments.of("NaN", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME)
+                Arguments.of("NaN", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                Arguments.of("-Infinity", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                Arguments.of("+Infinity", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                Arguments.of("0.1", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                Arguments.of("3.4028235E38", ParseFidelity.SAME, ParseFidelity.PARSE_FAIL, ParseFidelity.SAME),
+                Arguments.of("1.4E-45", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME),
+                Arguments.of("1.4798235515974676E-17", ParseFidelity.SAME, ParseFidelity.DIFFERENT, ParseFidelity.SAME)
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("provideTuplesForTestFidelity")
+    public void fidelity(String input,
+                         ParseFidelity strictFloatFidelity,
+                         ParseFidelity fastFloatFidelity,
+                         ParseFidelity doubleFidelity) {
 
-//
-//
-//
-//    testFidelity("-Infinity", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME);
-//        testFidelity("NaN", ParseFidelity.SAME, ParseFidelity.SAME, ParseFidelity.SAME);
-//
-//    }
+        Column<float[]> expectedFloat = Column.ofValues("Values", Float.parseFloat(input));
+        Column<double[]> expectedDouble = Column.ofValues("Values", Double.parseDouble(input));
 
-    @Test
-    public void testFloatStrict() throws CsvReaderException {
-        final String[] inputs = {
-                "NaN",
-                "-Infinity",
-                "+Infinity",
-                "0.1",
-                "3.4028235E38",  // Float.MaxValue
-                "1.4E-45",  // Float.MinValue
-                "1.4798235515974676E-17"
-        };
-
-        final float[] expectedValues = new float[inputs.length];
-        for (int i = 0; i != inputs.length; ++i) {
-            expectedValues[i] = Float.parseFloat(inputs[i]);
-        }
-
-        final String source = "Values\n" + String.join("\n", inputs);
-
-        final ColumnSet expected =
-                ColumnSet.of(
-                        Column.ofValues("Values", expectedValues));
-
-        final CsvSpecs specs = CsvTestUtil.defaultCsvBuilder().parsers(Collections.singletonList(
-                Parsers.FLOAT_STRICT)).build();
-        CsvTestUtil.invokeTest(specs, source, expected);
+        checkHelper(input, expectedFloat, Parsers.FLOAT_STRICT, strictFloatFidelity);
+        checkHelper(input, expectedFloat, Parsers.FLOAT_FAST, fastFloatFidelity);
+        checkHelper(input, expectedDouble, Parsers.DOUBLE, doubleFidelity);
     }
 
-    @Test
+    private static void checkHelper(String input, Column<?> expectedColumn, Parser<?> parser,
+                                    ParseFidelity fidelity) {
+        final String source = "Values\n" + input + "\n";
+        final ColumnSet expected = ColumnSet.of(expectedColumn);
+
+        final CsvSpecs specs = CsvTestUtil.defaultCsvBuilder().parsers(Collections.singletonList(parser)).build();
+        try {
+            CsvTestUtil.invokeTest(specs, source, expected);
+            Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.SAME);
+        } catch (CsvReaderException e) {
+            Assertions.assertThat(fidelity).isEqualTo(ParseFidelity.PARSE_FAIL);
+        }
+    }
+
     public void testFloatFast() throws CsvReaderException {
         final String[] inputs = {
                 "NaN",
